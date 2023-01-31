f522f335867d0026f8d4d6811437a75a9f176f76
CoderJava <kolonel.yudisetiawan@gmail.com>
2023-01-31 21:20:17 +0700

    Daftarkan use case `GetProject` kedalam mock_helper.dart

--- test/helper/mock_helper.dart
+++ test/helper/mock_helper.dart
@@ -5,6 +5,7 @@ import 'package:dipantau_desktop_client/core/util/helper.dart';
 import 'package:dipantau_desktop_client/core/util/shared_preferences_manager.dart';
 import 'package:dipantau_desktop_client/feature/data/datasource/general/general_remote_data_source.dart';
 import 'package:dipantau_desktop_client/feature/domain/repository/general/general_repository.dart';
+import 'package:dipantau_desktop_client/feature/domain/usecase/get_project/get_project.dart';
 import 'package:mockito/annotations.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -18,5 +19,6 @@ import 'package:shared_preferences/shared_preferences.dart';
   Helper,
   GeneralRemoteDataSource,
   GeneralRepository,
+  GetProject,
 ])
 void main() {}
