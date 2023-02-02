4e09ac59fb3c74da866cbe9044f8939a038db343
CoderJava <kolonel.yudisetiawan@gmail.com>
2023-02-02 20:05:08 +0700

    Buat beberapa function didalam helper.dart
    Buat variable `getDefaultPaddingLayout` dan `getDefaultWindowSize`. Lalu, buat function `setDateFormat` dan `convertSecondToTrackingTime`. Sekalian buat unit test-nya.

--- lib/core/util/helper.dart
+++ lib/core/util/helper.dart
@@ -1,6 +1,46 @@
+import 'package:dipantau_desktop_client/feature/data/model/tracking_time/tracking_time.dart';
+import 'package:intl/intl.dart';
+
 class Helper {
   bool checkValidationEmail(String email) {
-    var isEmailValid = RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9!#$%&'*+-/=?^_`{|}~]+\.[a-zA-Z]+").hasMatch(email);
+    final isEmailValid =
+        RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9!#$%&'*+-/=?^_`{|}~]+\.[a-zA-Z]+")
+            .hasMatch(email);
     return isEmailValid;
+  }
+
+  double get getDefaultPaddingLayout => 24.0;
+
+  double get getDefaultWindowSize => 500.0;
+
+  DateFormat setDateFormat(String pattern, {String? languageCode}) {
+    if (languageCode != null) {
+      return DateFormat(pattern, languageCode);
+    }
+    return DateFormat(pattern, 'en');
+  }
+
+  TrackingTime convertSecondToTrackingTime(int durationInSecond) {
+    var hour = 0;
+    var minute = 0;
+    var second = 0;
+    if (durationInSecond >= 3600) {
+      hour = durationInSecond ~/ 3600;
+      second = durationInSecond % 3600;
+      if (second >= 60) {
+        minute = second ~/ 60;
+        second = second % 60;
+      }
+    } else if (durationInSecond >= 60) {
+      minute = durationInSecond ~/ 60;
+      second = durationInSecond % 60;
+    } else {
+      second = durationInSecond;
+    }
+    return TrackingTime(
+      hour: hour,
+      minute: minute,
+      second: second,
+    );
   }
 }
--- test/util/helper_test.dart
+++ test/util/helper_test.dart
@@ -1,10 +1,14 @@
 import 'package:dipantau_desktop_client/core/util/helper.dart';
+import 'package:dipantau_desktop_client/feature/data/model/tracking_time/tracking_time.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   late Helper helper;
 
   setUp(() {
+    initializeDateFormatting('en', '');
     helper = Helper();
   });
 
@@ -20,6 +24,50 @@ void main() {
       expect(result, true);
       expect(result2, true);
       expect(result3, true);
+    },
+  );
+
+  test(
+    'pastikan output dari variable getDefaultPaddingLayout',
+    () async {
+      // assert
+      expect(helper.getDefaultPaddingLayout, 24.0);
+    },
+  );
+
+  test(
+    'pastikan output dari variable getDefaultWindowSize',
+    () async {
+      // assert
+      expect(helper.getDefaultWindowSize, 500.0);
+    },
+  );
+
+  test(
+    'pastikan fungsi setDateFormat bisa mengembalikan objek DateFormat sesuai dengan pattern yang diberikan',
+    () async {
+      // act
+      final result = helper.setDateFormat('dd');
+      final result2 = helper.setDateFormat('dd', languageCode: 'id');
+
+      // assert
+      expect(result.pattern, DateFormat('dd', 'en').pattern);
+      expect(result2.pattern, DateFormat('dd', 'id').pattern);
+    },
+  );
+
+  test(
+    'pastikan fungsi convertSecondToTrackingTime bisa mengembalikan waktu dalam satuan jam, menit, dan detik',
+    () async {
+      // act
+      final result = helper.convertSecondToTrackingTime(34);
+      final result2 = helper.convertSecondToTrackingTime(95);
+      final result3 = helper.convertSecondToTrackingTime(3666);
+
+      // assert
+      expect(result, TrackingTime(hour: 0, minute: 0, second: 34));
+      expect(result2, TrackingTime(hour: 0, minute: 1, second: 35));
+      expect(result3, TrackingTime(hour: 1, minute: 1, second: 6));
     },
   );
 }
