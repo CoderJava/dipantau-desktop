5a62d41a19859d81c70ba6a8aff846d85702fa8e
CoderJava <kolonel.yudisetiawan@gmail.com>
2023-02-18 11:34:27 +0700

    Daftarkan icon tray kedalam asset

---
+++ lib/core/util/images.dart
@@ -0,0 +1,7 @@
+class BaseIconTray {
+  static const _iconPath = 'assets/images';
+  static const iconIco = '$_iconPath/tray_icon.ico';
+  static const iconPng = '$_iconPath/tray_icon.png';
+  static const iconOriginalIco = '$_iconPath/tray_icon_original.ico';
+  static const iconOriginalPng = '$_iconPath/tray_icon_original.png';
+}
