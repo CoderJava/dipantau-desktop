aa7bc5a3f28a84b1ebc61762e9c72a7b62aff0e1
CoderJava <kolonel.yudisetiawan@gmail.com>
2023-02-18 23:36:22 +0700

    Buat class notification_helper.dart
    Class ini berfungsi sebagai centralize dari semua fitur yang berhubungan dengan fitur local notification.

---
+++ lib/core/util/notification_helper.dart
@@ -0,0 +1,33 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class NotificationHelper {
+  static FlutterLocalNotificationsPlugin? localNotification;
+
+  NotificationHelper() {
+    localNotification ??= FlutterLocalNotificationsPlugin();
+  }
+
+  void requestPermissionNotification() {
+    localNotification
+        ?.resolvePlatformSpecificImplementation<MacOSFlutterLocalNotificationsPlugin>()
+        ?.requestPermissions(
+          sound: true,
+          alert: true,
+        );
+  }
+
+  void showScreenshotTakenNotification() {
+    localNotification?.show(
+      DateTime.now().millisecond,
+      'app_name'.tr(),
+      'screenshot_taken'.tr(),
+      const NotificationDetails(
+        macOS: DarwinNotificationDetails(
+          presentAlert: true,
+          presentSound: true,
+        ),
+      ),
+    );
+  }
+}
