3f2a71b47e32763a2c115721685a7524292637ae
CoderJava <kolonel.yudisetiawan@gmail.com>
2023-02-18 23:35:30 +0700

    [macOS] Buat method channel untuk fitur take screenshot monitor

--- lib/core/util/method_channel_helper.dart
+++ lib/core/util/method_channel_helper.dart
@@ -1,9 +1,13 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:path_provider/path_provider.dart';
 
 class MethodChannelHelper {
   final channelName = 'dipantau/channel';
   final keyInvokeMethodQuitApp = 'quit_app';
+  final keyInvokeMethodTakeScreenshot = 'take_screenshot';
 
   late MethodChannel methodChannel;
 
@@ -17,5 +21,37 @@ class MethodChannelHelper {
     } catch (error) {
       debugPrint('Error quit app: $error');
     }
+  }
+
+  Future<void> doTakeScreenshot() async {
+    try {
+      final directory = await getApplicationDocumentsDirectory();
+      final directoryPath = '${directory.path}/dipantau';
+      final isDirectoryExists = checkDirectory(directoryPath);
+      if (isDirectoryExists) {
+        await methodChannel.invokeMethod(
+          keyInvokeMethodTakeScreenshot,
+          {
+            'path': directoryPath,
+          },
+        );
+      }
+    } catch (error) {
+      debugPrint('Error do take screenshot: $error');
+    }
+  }
+
+  bool checkDirectory(String pathDirectory) {
+    final directory = Directory(pathDirectory);
+    if (pathDirectory.isNotEmpty && !directory.existsSync()) {
+      try {
+        directory.createSync(recursive: true);
+        return true;
+      } catch (error) {
+        debugPrint('Error create directory: $error');
+        return false;
+      }
+    }
+    return true;
   }
 }
--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -19,7 +19,47 @@ class AppDelegate: FlutterAppDelegate {
             if ("quit_app" == call.method) {
                 NSApp.terminate(self)
                 result(true)
+            } else if ("take_screenshot" == call.method) {
+                let args: [String: Any] = call.arguments as! [String: Any]
+                let path: String = args["path"] as! String
+                self.takeScreenshots(folderName: path)
+                result(true)
             }
         })
+    }
+    
+    func takeScreenshots(folderName: String) {
+        var displayCount: UInt32 = 0
+        var result = CGGetActiveDisplayList(0, nil, &displayCount)
+        if (result != CGError.success) {
+            print("Error get active display list: \(result)")
+            return
+        }
+        
+        let allocated = Int(displayCount)
+        let activeDisplays = UnsafeMutablePointer<CGDirectDisplayID>.allocate(capacity: allocated)
+        result = CGGetActiveDisplayList(displayCount, activeDisplays, &displayCount)
+        if (result != CGError.success) {
+            print("Error get active display list 2: \(result)")
+            return
+        }
+        
+        for i in 1...displayCount {
+            let unixTimestamp = createTimestamp()
+            let fileUrl = URL(fileURLWithPath: folderName + "/\(unixTimestamp)" + "_" + "\(i)" + ".jpg", isDirectory: true)
+            let screenshot:CGImage = CGDisplayCreateImage(activeDisplays[Int(i-1)])!
+            let bitmapRep = NSBitmapImageRep(cgImage: screenshot)
+            let jpegData = bitmapRep.representation(using: NSBitmapImageRep.FileType.jpeg, properties: [:])!
+            
+            do {
+                try jpegData.write(to: fileUrl, options: .atomic)
+            } catch {
+                print("Error in looping display count: \(error)")
+            }
+        }
+    }
+    
+    func createTimestamp() -> Int32 {
+        return Int32(Date().timeIntervalSince1970)
     }
 }
