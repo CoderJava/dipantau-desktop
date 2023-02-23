7333a5f85ac30c39de50ad75f708d1795bba2a5c
CoderJava <kolonel.yudisetiawan@gmail.com>
2023-02-23 21:56:02 +0700

    Buat implement function endpoint `createTrackingData`
    Sekalian dengan unit test-nya.

--- lib/feature/data/repository/general/general_repository_impl.dart
+++ lib/feature/data/repository/general/general_repository_impl.dart
@@ -3,7 +3,9 @@ import 'package:dio/dio.dart';
 import 'package:dipantau_desktop_client/core/error/failure.dart';
 import 'package:dipantau_desktop_client/core/network/network_info.dart';
 import 'package:dipantau_desktop_client/feature/data/datasource/general/general_remote_data_source.dart';
+import 'package:dipantau_desktop_client/feature/data/model/general/general_response.dart';
 import 'package:dipantau_desktop_client/feature/data/model/project/project_response.dart';
+import 'package:dipantau_desktop_client/feature/data/model/tracking_data/tracking_data_body.dart';
 import 'package:dipantau_desktop_client/feature/domain/repository/general/general_repository.dart';
 
 class GeneralRepositoryImpl implements GeneralRepository {
@@ -31,6 +33,32 @@ class GeneralRepositoryImpl implements GeneralRepository {
     if (isConnected) {
       try {
         final response = await generalRemoteDataSource.getProject(email);
+        return Right(response);
+      } on DioError catch (error) {
+        if (error.response == null) {
+          return Left(ServerFailure(error.message));
+        }
+        final errorMessage = getErrorMessageFromEndpoint(
+          error.response?.data,
+          error.message,
+          error.response?.statusCode,
+        );
+        return Left(ServerFailure(errorMessage));
+      } on TypeError catch (error) {
+        final errorMessage = error.toString();
+        return Left(ParsingFailure(errorMessage));
+      }
+    } else {
+      return Left(ConnectionFailure());
+    }
+  }
+
+  @override
+  Future<Either<Failure, GeneralResponse>> createTrackingData(TrackingDataBody body) async {
+    final isConnected = await networkInfo.isConnected;
+    if (isConnected) {
+      try {
+        final response = await generalRemoteDataSource.createTrackingData(body);
         return Right(response);
       } on DioError catch (error) {
         if (error.response == null) {
