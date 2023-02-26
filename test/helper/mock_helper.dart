60c0d8f186ad17556a0c193a7dcf906f46330bf5
CoderJava <kolonel.yudisetiawan@gmail.com>
2023-02-26 10:45:37 +0700

    Daftarkan use case `CreateTrackingData` kedalam mock_helper.dart

--- test/helper/mock_helper.dart
+++ test/helper/mock_helper.dart
@@ -5,6 +5,7 @@ import 'package:dipantau_desktop_client/core/util/helper.dart';
 import 'package:dipantau_desktop_client/core/util/shared_preferences_manager.dart';
 import 'package:dipantau_desktop_client/feature/data/datasource/general/general_remote_data_source.dart';
 import 'package:dipantau_desktop_client/feature/domain/repository/general/general_repository.dart';
+import 'package:dipantau_desktop_client/feature/domain/usecase/create_tracking_data/create_tracking_data.dart';
 import 'package:dipantau_desktop_client/feature/domain/usecase/get_project/get_project.dart';
 import 'package:mockito/annotations.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -20,5 +21,6 @@ import 'package:shared_preferences/shared_preferences.dart';
   GeneralRemoteDataSource,
   GeneralRepository,
   GetProject,
+  CreateTrackingData,
 ])
 void main() {}
