3817bdc52e462d6cfa8f8bae16ffa015d795920d
CoderJava <kolonel.yudisetiawan@gmail.com>
2023-05-07 08:36:47 +0700

    Buat endpoint sign up
    Sekalian dengan unit test-nya.

--- lib/feature/data/datasource/auth/auth_remote_data_source.dart
+++ lib/feature/data/datasource/auth/auth_remote_data_source.dart
@@ -2,6 +2,8 @@ import 'package:dio/dio.dart';
 import 'package:dipantau_desktop_client/config/flavor_config.dart';
 import 'package:dipantau_desktop_client/feature/data/model/login/login_body.dart';
 import 'package:dipantau_desktop_client/feature/data/model/login/login_response.dart';
+import 'package:dipantau_desktop_client/feature/data/model/sign_up/sign_up_body.dart';
+import 'package:dipantau_desktop_client/feature/data/model/sign_up/sign_up_response.dart';
 
 abstract class AuthRemoteDataSource {
   /// Panggil endpoint [host]/auth/login
@@ -10,6 +12,13 @@ abstract class AuthRemoteDataSource {
   late String pathLogin;
 
   Future<LoginResponse> login(LoginBody body);
+
+  /// Panggil endpoint [host]/auth/signup
+  ///
+  /// Throws [DioError] untuk semua error kode
+  late String pathSignUp;
+
+  Future<SignUpResponse> signUp(SignUpBody body);
 }
 
 class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
@@ -35,6 +44,23 @@ class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
       return LoginResponse.fromJson(response.data);
     } else {
       throw DioError(requestOptions: RequestOptions(path: pathLogin));
+    }
+  }
+
+  @override
+  String pathSignUp = '';
+
+  @override
+  Future<SignUpResponse> signUp(SignUpBody body) async {
+    pathSignUp = '$baseUrl/signup';
+    final response = await dio.post(
+      pathSignUp,
+      data: body.toJson(),
+    );
+    if (response.statusCode.toString().startsWith('2')) {
+      return SignUpResponse.fromJson(response.data);
+    } else {
+      throw DioError(requestOptions: RequestOptions(path: pathSignUp));
     }
   }
 }
--- test/feature/data/datasource/auth/auth_remote_data_source_test.dart
+++ test/feature/data/datasource/auth/auth_remote_data_source_test.dart
@@ -5,6 +5,8 @@ import 'package:dipantau_desktop_client/config/flavor_config.dart';
 import 'package:dipantau_desktop_client/feature/data/datasource/auth/auth_remote_data_source.dart';
 import 'package:dipantau_desktop_client/feature/data/model/login/login_body.dart';
 import 'package:dipantau_desktop_client/feature/data/model/login/login_response.dart';
+import 'package:dipantau_desktop_client/feature/data/model/sign_up/sign_up_body.dart';
+import 'package:dipantau_desktop_client/feature/data/model/sign_up/sign_up_response.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 
@@ -102,6 +104,82 @@ void main() {
 
         // act
         final call = remoteDataSource.login(tBody);
+
+        // assert
+        expect(() => call, throwsA(const TypeMatcher<DioError>()));
+      },
+    );
+  });
+
+  group('sign up', () {
+    const tPathBody = 'sign_up_body.json';
+    final tBody = SignUpBody.fromJson(
+      json.decode(
+        fixture(tPathBody),
+      ),
+    );
+    const tPathResponse = 'sign_up_response.json';
+    final tResponse = SignUpResponse.fromJson(
+      json.decode(
+        fixture(tPathResponse),
+      ),
+    );
+
+    void setUpMockDioSuccess() {
+      final responsePayload = json.decode(fixture(tPathResponse));
+      final response = Response(
+        requestOptions: tRequestOptions,
+        data: responsePayload,
+        statusCode: 200,
+        headers: Headers.fromMap({
+          Headers.contentTypeHeader: [Headers.jsonContentType],
+        }),
+      );
+      when(mockDio.post(any, data: anyNamed('data'))).thenAnswer((_) async => response);
+    }
+
+    test(
+      'pastikan endpoint login benar-benar terpanggil dengan method POST',
+      () async {
+        // arrange
+        setUpMockDioSuccess();
+
+        // act
+        await remoteDataSource.signUp(tBody);
+
+        // assert
+        verify(mockDio.post('$baseUrl/signup', data: anyNamed('data')));
+      },
+    );
+
+    test(
+      'pastikan mengembalikan objek class model SignUpResponse ketika menerima respon sukses '
+      'dari endpoint',
+      () async {
+        // arrange
+        setUpMockDioSuccess();
+
+        // act
+        final result = await remoteDataSource.signUp(tBody);
+
+        // assert
+        expect(result, tResponse);
+      },
+    );
+
+    test(
+      'pastikan akan menerima exception DioError ketika menerima respon kegagalan dari endpoint',
+      () async {
+        // arrange
+        final response = Response(
+          requestOptions: tRequestOptions,
+          data: 'Bad Request',
+          statusCode: 400,
+        );
+        when(mockDio.post(any, data: anyNamed('data'))).thenAnswer((_) async => response);
+
+        // act
+        final call = remoteDataSource.signUp(tBody);
 
         // assert
         expect(() => call, throwsA(const TypeMatcher<DioError>()));
