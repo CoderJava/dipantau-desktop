985c3e60a9c3a85b4d10b0b701483354a294b6e7
CoderJava <kolonel.yudisetiawan@gmail.com>
2023-05-07 13:38:28 +0700

    Hapus penggunaan FirebaseAuth pada halaman splash_page.dart untuk menentukan apakah user sudah login atau belum

--- lib/feature/presentation/page/splash/splash_page.dart
+++ lib/feature/presentation/page/splash/splash_page.dart
@@ -1,7 +1,8 @@
+import 'package:dipantau_desktop_client/core/util/shared_preferences_manager.dart';
 import 'package:dipantau_desktop_client/feature/presentation/page/home/home_page.dart';
 import 'package:dipantau_desktop_client/feature/presentation/page/login/login_page.dart';
 import 'package:dipantau_desktop_client/feature/presentation/widget/widget_custom_circular_progress_indicator.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:dipantau_desktop_client/injection_container.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -16,17 +17,19 @@ class SplashPage extends StatefulWidget {
 }
 
 class _SplashPageState extends State<SplashPage> {
+  final sharedPreferencesManager = sl<SharedPreferencesManager>();
+
   @override
   void initState() {
     super.initState();
     Future.delayed(const Duration(seconds: 1)).then((_) {
-      final currentUser = FirebaseAuth.instance.currentUser;
-      if (currentUser == null) {
-        // Jika belum login maka, arahkan ke halaman login_page.dart
-        context.go(LoginPage.routePath);
-      } else {
+      final isLogin = sharedPreferencesManager.getBool(SharedPreferencesManager.keyIsLogin) ?? false;
+      if (isLogin) {
         // Jika sudah login maka, arahkan ke halaman home_page.dart
-        context.go(HomePage.routePath);
+        context.goNamed(HomePage.routePath);
+      } else {
+        // Jika belum login maka, arahkan ke halaman login_page.dart
+        context.goNamed(LoginPage.routePath);
       }
     });
   }
