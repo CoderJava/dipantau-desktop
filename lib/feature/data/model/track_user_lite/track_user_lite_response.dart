ccfcf31376edb11ade0967d741d6997192f2dd28
CoderJava <kolonel.yudisetiawan@gmail.com>
2023-05-14 10:51:53 +0700

    Buat class model track_user_lite_response.dart
    Sekalian dengan unit test-nya.

---
+++ lib/feature/data/model/track_user_lite/track_user_lite_response.dart
@@ -0,0 +1,73 @@
+import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'track_user_lite_response.g.dart';
+
+@JsonSerializable()
+class TrackUserLiteResponse extends Equatable {
+  @JsonKey(name: 'project_id')
+  final int? projectId;
+  @JsonKey(name: 'project_name')
+  final String? projectName;
+  @JsonKey(name: 'tracked_in_seconds')
+  final int? trackedInSeconds;
+  @JsonKey(name: 'list_tracks')
+  final List<ItemTrackUserLiteResponse> listTracks;
+
+  TrackUserLiteResponse({
+    required this.projectId,
+    required this.projectName,
+    required this.trackedInSeconds,
+    required this.listTracks,
+  });
+
+  factory TrackUserLiteResponse.fromJson(Map<String, dynamic> json) => _$TrackUserLiteResponseFromJson(json);
+
+  Map<String, dynamic> toJson() => _$TrackUserLiteResponseToJson(this);
+
+  @override
+  List<Object?> get props => [
+        projectId,
+        projectName,
+        trackedInSeconds,
+        listTracks,
+      ];
+
+  @override
+  String toString() {
+    return 'TrackUserLiteResponse{projectId: $projectId, projectName: $projectName, trackedInSeconds: $trackedInSeconds, '
+        'listTracks: $listTracks}';
+  }
+}
+
+@JsonSerializable()
+class ItemTrackUserLiteResponse extends Equatable {
+  @JsonKey(name: 'task_id')
+  final int? taskId;
+  @JsonKey(name: 'task_name')
+  final String? taskName;
+  @JsonKey(name: 'tracked_in_seconds')
+  final int? trackedInSeconds;
+
+  ItemTrackUserLiteResponse({
+    required this.taskId,
+    required this.taskName,
+    required this.trackedInSeconds,
+  });
+
+  factory ItemTrackUserLiteResponse.fromJson(Map<String, dynamic> json) => _$ItemTrackUserLiteResponseFromJson(json);
+
+  Map<String, dynamic> toJson() => _$ItemTrackUserLiteResponseToJson(this);
+
+  @override
+  List<Object?> get props => [
+        taskId,
+        taskName,
+        trackedInSeconds,
+      ];
+
+  @override
+  String toString() {
+    return 'ItemTrackUserLiteResponse{taskId: $taskId, taskName: $taskName, trackedInSeconds: $trackedInSeconds}';
+  }
+}
---
+++ test/feature/data/model/track_user_lite/track_user_lite_response_test.dart
@@ -0,0 +1,77 @@
+import 'dart:convert';
+
+import 'package:dipantau_desktop_client/feature/data/model/track_user_lite/track_user_lite_response.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import '../../../../fixture/fixture_reader.dart';
+
+void main() {
+  const tPathJson = 'track_user_lite_response.json';
+  final tModel = TrackUserLiteResponse.fromJson(
+    json.decode(
+      fixture(tPathJson),
+    ),
+  );
+
+  test(
+    'pastikan output dari nilai props',
+    () async {
+      // assert
+      expect(
+        tModel.props,
+        [
+          tModel.projectId,
+          tModel.projectName,
+          tModel.trackedInSeconds,
+          tModel.listTracks,
+        ],
+      );
+    },
+  );
+
+  test(
+    'pastikan output dari fungsi toString',
+    () async {
+      // assert
+      expect(
+        tModel.toString(),
+        'TrackUserLiteResponse{projectId: ${tModel.projectId}, projectName: ${tModel.projectName}, '
+        'trackedInSeconds: ${tModel.trackedInSeconds}, listTracks: ${tModel.listTracks}}',
+      );
+    },
+  );
+
+  test(
+    'pastikan fungsi fromJson bisa mengembalikan objek class model',
+    () async {
+      // arrange
+      final jsonData = json.decode(
+        fixture(tPathJson),
+      );
+
+      // act
+      final actualModel = TrackUserLiteResponse.fromJson(jsonData);
+
+      // assert
+      expect(actualModel, tModel);
+    },
+  );
+
+  test(
+    'pastikan fungsi toJson bisa mengembalikan objek map',
+    () async {
+      // arrange
+      final model = TrackUserLiteResponse.fromJson(
+        json.decode(
+          fixture(tPathJson),
+        ),
+      );
+
+      // act
+      final actualMap = json.encode(model.toJson());
+
+      // assert
+      expect(actualMap, json.encode(tModel.toJson()));
+    },
+  );
+}
