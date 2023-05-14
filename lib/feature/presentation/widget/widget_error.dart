c55ae9242cb4e325c70a6f924d437a74ca769b49
CoderJava <kolonel.yudisetiawan@gmail.com>
2023-05-15 00:00:27 +0700

    Ubah tampilan dari widget_error.dart

--- lib/feature/presentation/widget/widget_error.dart
+++ lib/feature/presentation/widget/widget_error.dart
@@ -3,11 +3,13 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
 class WidgetError extends StatelessWidget {
+  final String title;
   final String message;
   final Function()? onTryAgain;
 
   const WidgetError({
     Key? key,
+    required this.title,
     required this.message,
     this.onTryAgain,
   }) : super(key: key);
@@ -19,11 +21,23 @@ class WidgetError extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         mainAxisSize: MainAxisSize.min,
         children: [
-          Text(message),
-          onTryAgain == null ? Container() : WidgetPrimaryButton(
-            onPressed: onTryAgain,
-            child: Text('try_again'.tr()),
+          Text(
+            title,
+            style: Theme.of(context).textTheme.titleLarge,
           ),
+          const SizedBox(height: 8),
+          Text(
+            message,
+            style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+              color: Colors.grey[700],
+            ),
+          ),
+          onTryAgain == null
+              ? Container()
+              : WidgetPrimaryButton(
+                  onPressed: onTryAgain,
+                  child: Text('try_again'.tr()),
+                ),
         ],
       ),
     );
