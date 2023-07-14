f24f6d4a8b14c6a02c60a3dc7b258bd8971a8dcd
CoderJava <kolonel.yudisetiawan@gmail.com>
2023-07-14 10:19:37 +0700

    Gunakan variable reusable untuk url auto updater

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:ui';
 import 'package:auto_updater/auto_updater.dart';
 import 'package:dipantau_desktop_client/config/flavor_config.dart';
 import 'package:dipantau_desktop_client/core/util/enum/appearance_mode.dart';
+import 'package:dipantau_desktop_client/core/util/enum/global_variable.dart';
 import 'package:dipantau_desktop_client/core/util/shared_preferences_manager.dart';
 import 'package:dipantau_desktop_client/feature/data/model/user_profile/user_profile_response.dart';
 import 'package:dipantau_desktop_client/feature/presentation/bloc/appearance/appearance_bloc.dart';
@@ -62,10 +63,10 @@ import 'package:window_manager/window_manager.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  String feedURL = 'https://raw.githubusercontent.com/CoderJava/dipantau-desktop/main/dist/appcast.xml';
-  await autoUpdater.setFeedURL(feedURL);
-  await autoUpdater.checkForUpdates();
-  await autoUpdater.setScheduledCheckInterval(3600);
+  // Auto updater
+  const feedURL = autoUpdaterUrl;
+  autoUpdater.setFeedURL(feedURL);
+  autoUpdater.checkForUpdates();
 
   // Easy localization
   await EasyLocalization.ensureInitialized();
