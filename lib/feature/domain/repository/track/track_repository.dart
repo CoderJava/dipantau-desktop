9373a4580d889553d03c40f5b1c3f49377ffdb94
CoderJava <kolonel.yudisetiawan@gmail.com>
2023-08-28 19:43:30 +0700

    feat: Buat implement function endpoint `createManualTrack`
    Sekalian dengan unit test-nya.

--- lib/feature/domain/repository/track/track_repository.dart
+++ lib/feature/domain/repository/track/track_repository.dart
@@ -4,6 +4,7 @@ import 'package:dipantau_desktop_client/feature/data/model/create_track/bulk_cre
 import 'package:dipantau_desktop_client/feature/data/model/create_track/bulk_create_track_image_body.dart';
 import 'package:dipantau_desktop_client/feature/data/model/create_track/create_track_body.dart';
 import 'package:dipantau_desktop_client/feature/data/model/general/general_response.dart';
+import 'package:dipantau_desktop_client/feature/data/model/manual_create_track/manual_create_track_body.dart';
 import 'package:dipantau_desktop_client/feature/data/model/track_user/track_user_response.dart';
 import 'package:dipantau_desktop_client/feature/data/model/track_user_lite/track_user_lite_response.dart';
 
@@ -19,4 +20,6 @@ abstract class TrackRepository {
   Future<({Failure? failure, TrackUserResponse? response})> getTrackUser(String userId, String date);
 
   Future<({Failure? failure, GeneralResponse? response})> deleteTrackUser(int trackId);
+
+  Future<({Failure? failure, GeneralResponse? response})> createManualTrack(ManualCreateTrackBody body);
 }
