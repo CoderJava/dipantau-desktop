ea618913863bc0cc095431271580612d7f980c31
CoderJava <kolonel.yudisetiawan@gmail.com>
2023-09-01 21:07:58 +0700

    feat: Buat implement function endpoint `getProjectTaskByUserId`
    Sekalian dengan unit test-nya.

--- lib/feature/data/repository/project/project_repository_impl.dart
+++ lib/feature/data/repository/project/project_repository_impl.dart
@@ -4,6 +4,7 @@ import 'package:dipantau_desktop_client/core/error/failure.dart';
 import 'package:dipantau_desktop_client/core/network/network_info.dart';
 import 'package:dipantau_desktop_client/feature/data/datasource/project/project_remote_data_source.dart';
 import 'package:dipantau_desktop_client/feature/data/model/project/project_response.dart';
+import 'package:dipantau_desktop_client/feature/data/model/project_task/project_task_response.dart';
 import 'package:dipantau_desktop_client/feature/domain/repository/project/project_repository.dart';
 
 class ProjectRepositoryImpl implements ProjectRepository {
@@ -50,5 +51,35 @@ class ProjectRepositoryImpl implements ProjectRepository {
     } else {
       return Left(ConnectionFailure());
     }
+  }
+
+  @override
+  Future<({Failure? failure, ProjectTaskResponse? response})> getProjectTaskByUserId(String userId) async {
+    Failure? failure;
+    ProjectTaskResponse? response;
+    final isConnected = await networkInfo.isConnected;
+    if (isConnected) {
+      try {
+        response = await remoteDataSource.getProjectTaskByUserId(userId);
+      } on DioException catch (error) {
+        final message = error.message ?? error.toString();
+        if (error.response == null) {
+          failure = ServerFailure(message);
+        } else {
+          final errorMessage = getErrorMessageFromEndpoint(
+            error.response?.data,
+            message,
+            error.response?.statusCode,
+          );
+          failure = ServerFailure(errorMessage);
+        }
+      } on TypeError catch (error) {
+        final errorMessage = error.toString();
+        failure = ParsingFailure(errorMessage);
+      }
+    } else {
+      failure = ConnectionFailure();
+    }
+    return (failure: failure, response: response);
   }
 }
--- lib/feature/domain/repository/project/project_repository.dart
+++ lib/feature/domain/repository/project/project_repository.dart
@@ -1,7 +1,10 @@
 import 'package:dartz/dartz.dart';
 import 'package:dipantau_desktop_client/core/error/failure.dart';
 import 'package:dipantau_desktop_client/feature/data/model/project/project_response.dart';
+import 'package:dipantau_desktop_client/feature/data/model/project_task/project_task_response.dart';
 
 abstract class ProjectRepository {
   Future<Either<Failure, ProjectResponse>> getProject(String userId);
+
+  Future<({Failure? failure, ProjectTaskResponse? response})> getProjectTaskByUserId(String userId);
 }
