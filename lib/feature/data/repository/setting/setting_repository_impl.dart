fe00358e24101c80c72632d04eea0d6c23a39c6d
CoderJava <kolonel.yudisetiawan@gmail.com>
2023-09-10 22:10:46 +0700

    feat: Buat implement function endpoint `getUserSetting` dan `updateUserSetting`
    Sekalian dengan unit test-nya.

--- lib/feature/data/repository/setting/setting_repository_impl.dart
+++ lib/feature/data/repository/setting/setting_repository_impl.dart
@@ -5,6 +5,8 @@ import 'package:dipantau_desktop_client/feature/data/datasource/setting/setting_
 import 'package:dipantau_desktop_client/feature/data/model/all_user_setting/all_user_setting_response.dart';
 import 'package:dipantau_desktop_client/feature/data/model/kv_setting/kv_setting_body.dart';
 import 'package:dipantau_desktop_client/feature/data/model/kv_setting/kv_setting_response.dart';
+import 'package:dipantau_desktop_client/feature/data/model/user_setting/user_setting_body.dart';
+import 'package:dipantau_desktop_client/feature/data/model/user_setting/user_setting_response.dart';
 import 'package:dipantau_desktop_client/feature/domain/repository/setting/setting_repository.dart';
 
 class SettingRepositoryImpl implements SettingRepository {
@@ -94,6 +96,66 @@ class SettingRepositoryImpl implements SettingRepository {
     if (isConnected) {
       try {
         response = await remoteDataSource.getAllUserSetting();
+      } on DioException catch (error) {
+        final message = error.message ?? error.toString();
+        if (error.response == null) {
+          failure = ServerFailure(message);
+        } else {
+          final errorMessage = getErrorMessageFromEndpoint(
+            error.response?.data,
+            message,
+            error.response?.statusCode,
+          );
+          failure = ServerFailure(errorMessage);
+        }
+      } on TypeError catch (error) {
+        final errorMessage = error.toString();
+        failure = ParsingFailure(errorMessage);
+      }
+    } else {
+      failure = ConnectionFailure();
+    }
+    return (failure: failure, response: response);
+  }
+
+  @override
+  Future<({Failure? failure, UserSettingResponse? response})> getUserSetting() async {
+    Failure? failure;
+    UserSettingResponse? response;
+    final isConnected = await networkInfo.isConnected;
+    if (isConnected) {
+      try {
+        response = await remoteDataSource.getUserSetting();
+      } on DioException catch (error) {
+        final message = error.message ?? error.toString();
+        if (error.response == null) {
+          failure = ServerFailure(message);
+        } else {
+          final errorMessage = getErrorMessageFromEndpoint(
+            error.response?.data,
+            message,
+            error.response?.statusCode,
+          );
+          failure = ServerFailure(errorMessage);
+        }
+      } on TypeError catch (error) {
+        final errorMessage = error.toString();
+        failure = ParsingFailure(errorMessage);
+      }
+    } else {
+      failure = ConnectionFailure();
+    }
+    return (failure: failure, response: response);
+  }
+
+  @override
+  Future<({Failure? failure, bool? response})> updateUserSetting(UserSettingBody body) async {
+    Failure? failure;
+    bool? response;
+    final isConnected = await networkInfo.isConnected;
+    if (isConnected) {
+      try {
+        response = await remoteDataSource.updateUserSetting(body);
       } on DioException catch (error) {
         final message = error.message ?? error.toString();
         if (error.response == null) {
--- lib/feature/domain/repository/setting/setting_repository.dart
+++ lib/feature/domain/repository/setting/setting_repository.dart
@@ -2,6 +2,8 @@ import 'package:dipantau_desktop_client/core/error/failure.dart';
 import 'package:dipantau_desktop_client/feature/data/model/all_user_setting/all_user_setting_response.dart';
 import 'package:dipantau_desktop_client/feature/data/model/kv_setting/kv_setting_body.dart';
 import 'package:dipantau_desktop_client/feature/data/model/kv_setting/kv_setting_response.dart';
+import 'package:dipantau_desktop_client/feature/data/model/user_setting/user_setting_body.dart';
+import 'package:dipantau_desktop_client/feature/data/model/user_setting/user_setting_response.dart';
 
 abstract class SettingRepository {
   Future<({Failure? failure, KvSettingResponse? response})> getKvSetting();
@@ -9,4 +11,8 @@ abstract class SettingRepository {
   Future<({Failure? failure, bool? response})> setKvSetting(KvSettingBody body);
 
   Future<({Failure? failure, AllUserSettingResponse? response})> getAllUserSetting();
+
+  Future<({Failure? failure, UserSettingResponse? response})> getUserSetting();
+
+  Future<({Failure? failure, bool? response})> updateUserSetting(UserSettingBody body);
 }
