86dab70841fd3c48b9d3748f861b335e28acc810
CoderJava <kolonel.yudisetiawan@gmail.com>
2023-11-26 19:12:32 +0700

    feat: Buat widget_loading_center_full_screen.dart
    Widget tersebut berfungsi untuk menampilkan loading center full screen overlay

---
+++ lib/feature/presentation/widget/widget_loading_center_full_screen.dart
@@ -0,0 +1,16 @@
+import 'package:dipantau_desktop_client/feature/presentation/widget/widget_custom_circular_progress_indicator.dart';
+import 'package:flutter/material.dart';
+
+class WidgetLoadingCenterFullScreen extends StatelessWidget {
+  const WidgetLoadingCenterFullScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      color: Colors.black.withOpacity(.5),
+      child: const WidgetCustomCircularProgressIndicator(),
+    );
+  }
+}
