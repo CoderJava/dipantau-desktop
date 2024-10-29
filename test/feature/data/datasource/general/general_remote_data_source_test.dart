8a6369fca3a26907745ff25e42a617c5eae2dc51
CoderJava <kolonel.yudisetiawan@gmail.com>
2024-10-29 08:23:31 +0700

    Buat endpoint `ping`
    Sekalian buat unit testnya.

---
+++ test/feature/data/datasource/general/general_remote_data_source_test.dart
@@ -0,0 +1,108 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
+import 'package:dipantau_desktop_client/config/flavor_config.dart';
+import 'package:dipantau_desktop_client/feature/data/datasource/general/general_remote_data_source.dart';
+import 'package:dipantau_desktop_client/feature/data/model/general/general_response.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+import '../../../../fixture/fixture_reader.dart';
+import '../../../../helper/mock_helper.mocks.dart';
+
+void main() {
+  late GeneralRemoteDataSource remoteDataSource;
+  late MockDio mockDio;
+  late MockHttpClientAdapter mockDioAdapter;
+
+  const baseUrl = 'https://example.com';
+
+  setUp(() {
+    FlavorConfig(
+      values: FlavorValues(
+        baseUrl: baseUrl,
+        baseUrlAuth: '',
+        baseUrlUser: '',
+        baseUrlTrack: '',
+        baseUrlProject: '',
+        baseUrlSetting: '',
+      ),
+    );
+    mockDio = MockDio();
+    mockDioAdapter = MockHttpClientAdapter();
+    mockDio.httpClientAdapter = mockDioAdapter;
+    remoteDataSource = GeneralRemoteDataSourceImpl(dio: mockDio);
+  });
+
+  final tRequestOptions = RequestOptions(path: '');
+
+  group('ping', () {
+    const tPathResponse = 'general_response.json';
+    final tResponse = GeneralResponse.fromJson(
+      json.decode(
+        fixture(tPathResponse),
+      ),
+    );
+
+    void setUpMockDioSuccess() {
+      final responsePayload = json.decode(fixture(tPathResponse));
+      final response = Response(
+        requestOptions: tRequestOptions,
+        data: responsePayload,
+        statusCode: 200,
+        headers: Headers.fromMap({
+          Headers.contentTypeHeader: [Headers.jsonContentType],
+        }),
+      );
+      when(mockDio.get(any)).thenAnswer((_) async => response);
+    }
+
+    test(
+      'pastikan endpoint ping benar-benar terpanggil dengan method GET',
+      () async {
+        // arrange
+        setUpMockDioSuccess();
+
+        // act
+        await remoteDataSource.ping();
+
+        // assert
+        verify(mockDio.get('$baseUrl/api/ping'));
+      },
+    );
+
+    test(
+      'pastikan mengembalikan objek class model GeneralResponse ketika menerima respon sukses '
+      'dari endpoint',
+      () async {
+        // arrange
+        setUpMockDioSuccess();
+
+        // act
+        final result = await remoteDataSource.ping();
+
+        // assert
+        expect(result, tResponse);
+      },
+    );
+
+    test(
+      'pastikan akan menerima exception DioError ketika menerima respon kegagalan dari endpoint',
+      () async {
+        // arrange
+        final response = Response(
+          requestOptions: tRequestOptions,
+          data: 'Bad Request',
+          statusCode: 400,
+        );
+        when(mockDio.get(any)).thenAnswer((_) async => response);
+
+        // act
+        final call = remoteDataSource.ping();
+
+        // assert
+        expect(() => call, throwsA(const TypeMatcher<DioException>()));
+      },
+    );
+  });
+}
